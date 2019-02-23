197d86f3edb62676f9b5fa45207019cb6d723d59
frog-garden <47462942+frog-garden@users.noreply.github.com>
2019-02-23 17:33:24 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     elif message.content.startswith("/gather"):
-            await client.send_message(message.channel, random.choice([ "```yaml \n ------------------------------------------------------- \n What the...? A Perfectly Round Pebble sits by your paws! (1 point) \n ------------------------------------------------------- \n ```",]))
+            await client.send_message(message.channel, random.choice([ "```yaml ------------------------------------------------------- \n What the...? A Perfectly Round Pebble sits by your paws! (1 point) \n -------------------------------------------------------  ```",]))
             
 client.run("NTQ4NjQxOTkyODc2MjI4NjM4.D1ISMQ.z6aZL4bZzJ3ypn0fCMhgXYwAreE")
 client.login(process.env.BOT_TOKEN); 
